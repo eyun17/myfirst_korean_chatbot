d277819d960e6113387bdf2d506a65b267f4d005
Eunieversity <eyun@uni-osnabrueck.de>
2024-07-26 17:32:08 +0200

    test dir updated

---
+++ test/model_ner_test.py
@@ -0,0 +1,11 @@
+from utils.Preprocessing import Preprocess
+from models.ner.NerModel import NerModel
+
+p = Preprocess(word2index_dic='../train_tools/dict/chatbot_dict.bin',
+               userdic='../utils/user_dic.tsv')
+
+ner = NerModel(model_name='../models/ner/ner_model.h5', proprecess=p)
+
+query = '오늘 오전 13시 2분에 탕수육 주문하고 싶어요'
+predicts = ner.predict(query)
+print(predicts)
