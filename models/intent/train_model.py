8e54eb8337248e5375342af6cfc76cc862333dd7
Eunieversity <eyun@uni-osnabrueck.de>
2024-08-17 12:00:02 +0200

    edit intent/train_model.py

--- models/intent/train_model.py
+++ models/intent/train_model.py
@@ -1,126 +1,101 @@
 """
-챗본 엔진 NER 모델
+챗본 엔진 Intent 분류 모델
 """
 
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import preprocessing
-from sklearn.model_selection import train_test_split
-import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, Dense, Dropout, Conv1D, GlobalMaxPool1D, concatenate
+
+# Read Data
+train_file = "total_train_data.csv"
+data = pd.read_csv(train_file, delimiter=',')
+queries = data['query'].tolist()
+intents = data['intetn'].tolist()
+
 from utils.Preprocessing import Preprocess
-
-# 학습 파일 불러오기
-def read_file(file_name):
-    sents = []
-    with open(file_name, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        for idx, l in enumerate(lines):
-            if l[0] == ';' and lines [idx+1][0] == '$':
-                this_sent = []
-            elif l[0] == '$' and lines [idx-1][0] == ';':
-                continue
-            elif l[0] == '\n':
-                sents.append(this_sent)
-            else:
-                this_sent.append(tuple(l.split()))
-    return sents
-
-# 전처리 객체 생성
 p = Preprocess(word2index_dic='../../train_tools/dict/chatbot_dict.bin',
-              userdic='../../utils/user_dic.tsv')
+               userdic='../../utils/user_dic.tsv')
 
-# 학습용 말뭉치 데이터 불러오기
-corpus = read_file('ner_train.txt')
+# 단어 시퀀스 생성
+sequences = []
+for sentence in queries:
+    pos = p.pos(sentence)
+    keywords = p.get_keywords(pos, without_tag=True)
+    seq = p.get_wordidx_sequence(keywords)
+    sequences.append(seq)
 
-# 말뭉치 데이터에서 단어와 BIO 태그만 불러와 학습용 데이터셋 생성
-sentences, tags = [], []
-for t in corpus:
-    tagged_sentence = []
-    sentence, bio_tag = [], []
-    for w in t:
-        tagged_sentence.append((w[1], w[3]))
-        sentence.append(w[1])
-        bio_tag.append(w[3])
+# 단어 인덱스 시퀀스 벡터 생성
+# 단어 시퀀스 벡터 크기
+from config.GlobalParams import MAX_SEQ_LEN
+padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN, padding='post')
 
-    sentences.append(sentence)
-    tags.append(bio_tag)
+# 학습용, 검증용, 테스트용 데이터셋 생성 7:2:1
+ds = tf.data.Dataset.from_tensor_slices((padded_seqs, intents))
+ds = ds.shuffle(len(queries))
 
-print("샘플 크기 : \n", len(sentences))
-print("0번째 샘플 단어 시퀀스 : \n", sentences[0])
-print("0번째 샘플 bio 태그 : \n", tags[0])
-print("샘플 단어 시퀀스 최대 길이 : ", max(len(l) for l in sentences))
-print("샘플 단어 시퀀스 평균 길이 : ", (sum(map(len, sentences))/len(sentences)))
+train_size = int(len(padded_seqs) * 0.7)
+val_size = int(len(padded_seqs) * 0.2)
+test_size = int(len(padded_seqs) * 0.1)
 
-# 토크나이저 정의
-tag_tokenizer = preprocessing.text.Tokenizer(lower=False)
-#태그 정보는 lower=False 소문자로 변환하지 않는다
-tag_tokenizer.fit_on_texts(tags)
+train_ds = ds.take(train_size).batch(20)
+val_ds = ds.skip(train_size).take(val_size).batch(20)
+test_ds = ds.skip(train_size + val_size).take(test_size).batch(20)
 
-# 단어 사전 및 태그 사전 크기
-vocab_size = len(p.word_index) + 1
-tag_size = len(tag_tokenizer.word_index) + 1
-print("BIO 태그 사전 크기:" , tag_size)
-print("단ㄴ어 사전 크기:", vocab_size)
+# 하이퍼파라미터 설정
+dropout_prob = 0.5
+EMB_SIZE = 128
+EPOCH = 5
+VOCAB_SIZE = len(p.word_index) + 1 #전체 단어 수
 
-# 학습용 단어 시퀀스 생성
-x_train = [p.get_wordidx_sequence(sent) for sent in sentences]
-y_train = tag_tokenizer.texts_to_sequences(tags)
+# CNN 모델 정의
+input_layer = Input(shape=(MAX_SEQ_LEN, ))
+embedding_layer = Embedding(VOCAB_SIZE, EMB_SIZE, input_length=MAX_SEQ_LEN)(input_layer)
+dropout_emb = Dropout(rate=dropout_prob)(embedding_layer)
 
-index_to_ner = tag_tokenizer.idex_word # 시퀀스 인덱스를 ner로 변환하기 위해 사용
-index_to_ner[0] = 'PAD'
+conv1 = Conv1D(
+    filters=128,
+    kernel_size = 3,
+    padding='valid',
+    activation = tf.nn.relu)(dropout_emb)
+pool1 = GlobalMaxPool1D()(conv1)
 
-# 시퀀스 패딩 처리
-max_len = 40
-x_train = preprocessing.sequence.pad_sequences(x_train, padding='post', maxlen=max_len)
-y_train = preprocessing.sequence.pad_sequences(y_train, padding='post', maxlen=max_len)
+conv2 = Conv1D(
+    filters=128,
+    kernel_size = 4,
+    padding='valid',
+    activation = tf.nn.relu)(dropout_emb)
+pool2 = GlobalMaxPool1D()(conv2)
 
-# 학습 데이터와 테스트 데이터를 8:2 비율로 분리
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, rtest_size=.2, random_state=1234)
+conv3 = Conv1D(
+    filters=128,
+    kernel_size = 5,
+    padding='valid',
+    activation = tf.nn.relu)(dropout_emb)
+pool3 = GlobalMaxPool1D()(conv3)
 
-# 출력 데이터를 원-핫 인코딩
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=tag_size)
-y_test = tf.keras.utils.to_categorical(y_test, num_classes=tag_size)
+# 3, 4, 5-gram 이후 합치기
+concat = concatenate([pool1, pool2, pool3])
 
-print("학습 샘플 시퀀스 형상 : ", x_train.shape)
-print("학습 샘플 레이블 형상 : ", y_train.shape)
-print("테스트 샘플 시퀀스 형상 : ", x_test.shape)
-print("테스트 샘플 레이블 형상 : ", y_test.shape)
+hidden = Dense(128, activation=tf.nn.relu)(concat)
+dropout_hidden = Dropout(rate=dropout_prob)(hidden)
+logits = Dense(5, name='logits')(dropout_hidden)
+predictions = Dense(5, activation =tf.nn.softmax)(logits)
 
+# 모델 생성
+model = Model(inputs=input_layer, outputs=predictions)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-# 모델 정의
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
-from tensorflow.keras.optimizers import Adam
+# 모델 학습
+model.fit(train_ds, validation_data=val_ds, epochs=EPOCH, verbose=1)
 
-model = Sequential()
-model.add(Embedding(input_dim=vocab_size, ouptut_dim=30, input_length=max_len, mask_zero=True))
-model.add(Bidirectional(LSTM(200, return_sequences=True, dropout=0.50, recurrent_dropout=0.25)))
-model.add(TimeDistributed(Dense(tag_size, activation='softmax')))
-model.compile(loss='categorical_crossentropy', optimier=Adam(0.01), metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size=128, epochs=10)
+# 모델 평가 (테스트 데이터셋 이용)
+loss, accuracy = model.evaluate(test_ds, verbose=1)
+print('Accuracy: %f' % (accuracy * 100))
+print('loss: %f' % (loss))
 
-print("평가 결과 : ", model.evaluate(x_test, y_test)[1])
-model.save('ner_model.h5')
-
-# 시퀀스를 NER 태그로 변환
-def sequences_to_tag(sequences): # 에측값을 index_to_ner를 사용하여 태깅 정보로 변경하는 함수
-    result = []
-    for sequence in sequences:  # 전체 시퀀스로부터 시퀀스를 하나씩 꺼낸다
-        temp = []
-        for pred in sequence:   # 시퀀스로부터 에측값을 하나씩 꺼낸다
-            pred_index = np.argmax(pred) # 예를 들어 [0, 0, 1, 0, 1] 이라면 1의 인덱스인 2를 리턴한다.
-            temp.append(index_to_ner[pred_index].replace("PAD", "0")) # 'PAD'는 '0'으로 변경
-        result.append(temp)
-    return result
-
-
-# F1 스코어 게산을 위해 사용
-from seqeval.metrics import f1_score, classification_report
-
-# 테스트 데이터셋의 ner 예측
-y_predicted = model.predict(x_test)
-pred_tags = sequences_to_tag(y_predicted)   # 예측된 NER
-test_tags = sequences_to_tag(y_test)    # 실제 NER
-
-# F1 평가 결과
-print(classification_report(test_tags, pred_tags))
-print("F1-score: {:.1%}".format(f1_score(test_tags, pred_tags)))
+# 모델 저장
+model.save('intent_model.h5')
